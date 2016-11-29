32785f532e85017d9b9587dab767ffd1ff9540c9
Julius <julius@uusinarkaus.com>
2016-11-29 19:22:40 +0200

    Initialized maven stuff!

---
+++ src/main/java/karhu/OispaKaljaaApplication.java
@@ -0,0 +1,12 @@
+package karhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OispaKaljaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OispaKaljaaApplication.class, args);
+    }
+}
