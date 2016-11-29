402e0e211b721194f97b78e0ae3b7309ec5bd4b0
hanninev <hanninev@fuxi16>
2016-11-29 23:00:12 +0200

    + findByBar

--- src/main/java/fi/oispakaljaa/karhu/repository/DrinkRepository.java
+++ src/main/java/fi/oispakaljaa/karhu/repository/DrinkRepository.java
@@ -1,9 +1,11 @@
 package fi.oispakaljaa.karhu.repository;
 
+import fi.oispakaljaa.karhu.domain.Bar;
 import fi.oispakaljaa.karhu.domain.Drink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DrinkRepository extends JpaRepository<Drink, Long> {
+            Bar findByBar(Bar bar);
 }
 
 
