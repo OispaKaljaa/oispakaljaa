d3622aa07f42def296659247784ca19f8fbe22fe
Hugo Holmqvist <hugo.holm@live.com>
2016-12-09 16:36:30 +0200

    Refactor code

--- src/main/java/fi/oispakaljaa/karhu/repository/BarRepository.java
+++ src/main/java/fi/oispakaljaa/karhu/repository/BarRepository.java
@@ -4,7 +4,6 @@ import fi.oispakaljaa.karhu.domain.Bar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BarRepository extends JpaRepository<Bar, Long> {
-    //ÄHHH MITEN TÄÄ KANTSIS TEHÄ SILLEEN ET PYSTYY TEHÄ FIND BY DRINK
 }
 
 
--- src/main/java/fi/oispakaljaa/karhu/repository/DrinkRepository.java
+++ src/main/java/fi/oispakaljaa/karhu/repository/DrinkRepository.java
@@ -5,7 +5,7 @@ import fi.oispakaljaa.karhu.domain.Drink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DrinkRepository extends JpaRepository<Drink, Long> {
-            Bar findByBar(Bar bar);
+    Bar findByBar(Bar bar);
 }
 
 
