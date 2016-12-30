1b0c75ce859eee817d03889d325f7af44b9f3cc2
Ville <ville.hanninen@helsinki.fi>
2016-12-30 16:23:12 +0200

    Couples tests added

--- src/test/java/fi/oispakaljaa/karhu/controller/DrinkControllerTest.java
+++ src/test/java/fi/oispakaljaa/karhu/controller/DrinkControllerTest.java
@@ -3,26 +3,27 @@ package fi.oispakaljaa.karhu.controller;
 import fi.oispakaljaa.karhu.repository.AccountRepository;
 import fi.oispakaljaa.karhu.repository.BarRepository;
 import fi.oispakaljaa.karhu.repository.DrinkRepository;
+import java.util.Random;
+import java.util.UUID;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-
-/**
- * Created by julkku on 12/18/16.
- */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DrinkControllerTest {
-
 
     @Autowired
     private AccountRepository accountRepository;
@@ -38,44 +39,42 @@ public class DrinkControllerTest {
     @Autowired
     private ListableBeanFactory listableBeanFactory;
 
-
     private MockMvc mockMvc;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+
+        String name = UUID.randomUUID().toString().substring(0, 6);
+        String address = UUID.randomUUID().toString().substring(0, 6);
+
+        mockMvc.perform(post("/api/bars/").contentType(MediaType.APPLICATION_JSON).content("{\"name\":\"" + name + "\",\n\"address\":\"" + address + "\"}"))
+                .andExpect(status().isOk());
     }
 
     @Test
     public void apiWorks() throws Exception {
-//        mockMvc.perform(get("/api/drinks"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType("application/json;charset=UTF-8"));
+        mockMvc.perform(get("/api/bars/1/drinks"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
 
     }
-//
-//    @Test
-//    public void listDrinksByBar() throws Exception {
-//
-//    }
-//
-//@Test
-//public void postDrink() throws Exception {
-//    String name = "Karjala";
-//
-//
-//    mockMvc.perform(post("/api/messages").content("{\"message\":\"" + message + "\",\"person\":\"" + person + "\"}"))
-//            .andExpect(status().is2xxSuccessful());
-//}
-//
-//    @Test
-//    public void getDrink() throws Exception {
-//
-//    }
-//
-//    @Test
-//    public void deleteDrink() throws Exception {
-//
-//    }
+
+    @Test
+    public void postDrink() throws Exception {
+        String drinkName = UUID.randomUUID().toString().substring(0, 6);
+        Random random = new Random();
+        Integer price = random.nextInt(5);
+        Integer volume = random.nextInt(5);
+        Integer alcoholPercentage = random.nextInt(500);
+
+        mockMvc.perform(post("/api/bars/1/drinks").contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"" + drinkName + "\",\n\"drinkType\":\"beer\",\n\"price\":\"" + price + "\",\n\"volume\":\"" + volume + "\",\n\"alcoholPercentage\":\"" + alcoholPercentage + "\"}"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/bars/1/drinks"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+    }
 
 }
